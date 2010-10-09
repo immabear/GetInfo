27f4a7e179f1aa4c4b2e731ea88e8d3e3cf37dd5
immabear <crackopenabear@hotmail.com>
2010-10-08 23:33:26 -0700

    Intial Commit.

---
+++ btjunkie.js
@@ -0,0 +1,52 @@
+ClientsU[1]="rss~http://btjunkie.org/rss.xml?query=|";
+function parsebtjunkie(id,data){
+ var title=[]; var seed=[]; var leech=[]; var cat=[]; var size=[]; var com=[];var loc=[];var tablerow; var tablecd='';
+ $.each(data.query.results.item,function(index){
+  title[index]=(this).title.split("  [")[0];
+  loc[index]=(this).link.replace("http://btjunkie.org/","http://dl.btjunkie.org/")+"/download.torrent";
+  leech[index]=(this).title.split("  [")[1].split("/")[0];
+  seed[index]=(this).title.split("  [")[1].split("/")[1].split("]")[0];
+  cat[index]=(this).description.split(" ")[1];
+  size[index]=(this).description.split(" ")[3];
+  com[index]=
+   '<table style="margin:0;width:100%">'+
+    '<tr>'+
+	 '<td class="comoverlap">Seeds: '+seed[index]+' | Leechs: '+leech[index]+'</td>'+
+	'</tr>'+
+	'<tr>'+
+	 '<td class="comoverlap">Size: '+size[index]+'</td>'+
+	'</tr>'+
+	'<tr>'+
+	 '<td class="comoverlap">Category: '+cat[index]+'</td>'+
+	'</tr>'+
+   '</table>';
+  tablerow=
+   '<div id="result'+index+'" class="resultimg" style="width:160;height:90">'+
+   com[index]+
+   '</div>'+
+   '<div class="tooltipbig">'+
+    '<table>'+
+     '<tr>'+
+      '<td style="height:80px"><b id="searchresult'+index+'">'+title[index]+'</b>'+
+	  '</td>'+
+	 '</tr>'+
+	 '<tr>'+
+	  '<td>'+
+	   '<button onclick="IMDb(\'searchresult'+index+'\')" class="buttonbig">IMDb</button>'+
+	   '<button onclick="downloadbtjunkie(\''+loc[index]+'\');" class="buttonbig">Info/Download</button>'+
+	  '</td>'+
+	 '</tr>'+
+	'</table>'+
+   '</div>';
+  tablecd=tablecd+tablerow; 
+ });
+ document.getElementById(id).innerHTML=tablecd;
+ aligntd();
+ $('#searchbox div[id]').tooltip({effect: 'slide',offset: [27, 10],relative: 'true'});
+ var title=[]; var seed=[]; var leech=[]; var cat=[]; var size=[]; var com=[];var loc=[];var tablerow; var tablecd='';
+}
+function downloadbtjunkie(loc){
+ minmaximize(0);
+ document.getElementById('searchframe').innerHTML="<iframe src='"+loc+"' class='searchframe' frameborder='0'></frame>";
+ $("#searchframe").show(600);
+}
