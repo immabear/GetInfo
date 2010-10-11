e1182b23476c5054697101deff32ddcf92fca9b1
immabear <crackopenabear@hotmail.com>
2010-10-10 17:25:07 -0700

    tt

--- jquery.xdomainajax.js
+++ jquery.xdomainajax.js
@@ -17,6 +17,8 @@ jQuery.ajax = (function(_ajax){
         exRegex = RegExp(protocol + '//' + hostname),
         YQL = 'http' + (/^https/.test(protocol)?'s':'') + '://query.yahooapis.com/v1/public/yql?callback=?',
         query = 'select {SELECT} from html where url="{URL}" and xpath="{XPATH}"';
+		
+	alert(query);
     
     function isExternal(url) {
         return !exRegex.test(url) && /:\/\//.test(url);
