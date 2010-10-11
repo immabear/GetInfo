d1460e916440fa2a12b1029760e14a4b424bc80b
immabear <crackopenabear@hotmail.com>
2010-10-11 11:34:50 -0700

    v

--- btjunkie.js
+++ btjunkie.js
@@ -1,6 +1,6 @@
 ClientsU[1]="html~http://btjunkie.org/search?q=|";
 ClientsS[1]='*'; //p.a.href, p.a.content, font.content
-ClientsX[1]='//div[@id="main"]/table[1]/tr/th/table/tr/th/table/*';
+ClientsX[1]='//div[@id="main"]/table[1]/tr/th/table/tr/th/table/tr';
 function parsebtjunkie(id,data){
  var idobj=document.getElementById(id);
  idobj.innerHTML='<textarea style="width:100%;height:100%">'+data+'</textarea>';
