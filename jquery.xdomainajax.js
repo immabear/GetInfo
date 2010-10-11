86a4274a54669c1fd1ae1cdc87f267a2e5923335
immabear <crackopenabear@hotmail.com>
2010-10-11 12:20:44 -0700

    v

--- jquery.xdomainajax.js
+++ jquery.xdomainajax.js
@@ -32,11 +32,10 @@ jQuery.ajax = (function(_ajax){
             
             o.url = YQL;
             o.dataType = 'json';
-            var querysx = query.replace("{SELECT}",o.select);
-			querysx = querysx.replace("{XPATH}",o.xpath);
-			querysx = querysx.replace("{URL}",url+(o.data ? (/\?/.test(url) ? '&' : '?')+jQuery.param(o.data) : ''));
+            var querysx = query.replace("{SELECT}",o.select).replace("{XPATH}",o.xpath).replace("{URL}",url+(o.data ? (/\?/.test(url) ? '&' : '?')+jQuery.param(o.data) : ''));
+			alert(querysx);
             o.data = {
-                q: encodeURIComponent(querysx),
+                q: querysx,
                 format: 'xml'
             };
             
