d6a493b1a0e5e3ccd0c3b5a19a93c3da3dab7b11
immabear <crackopenabear@hotmail.com>
2010-10-12 12:10:36 -0700

    c

--- btjunkie.js
+++ btjunkie.js
@@ -8,7 +8,7 @@ function parsebtjunkie(id,data){
  var dataarray=[], title=[], temp=[], seed=[], leech=[], cat=[], size=[], date=[], com=[], loc=[], tablerow, tablecd='', idobj=document.getElementById(id);
  dataarray=data.split("<p");
  $.each(dataarray,function(index, value){
-  if(index!==0&&index<((dataarray.length)-1)){
+  if(index!==0&&index<((dataarray.length)-3)){
    dataarray[index]='<p'+value;
    dataarray[index]=dataarray[index].replace(/p>(\s+)</g,'').replace(/\s+/g,' ').replace(/<\/strong>/,'<strong>|');
    alert(dataarray[index]);
