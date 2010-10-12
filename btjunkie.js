fe113ce15e6158fdfc5a1296ed7e5cc73ae7e5f9
immabear <crackopenabear@hotmail.com>
2010-10-12 05:00:34 -0700

    n

--- btjunkie.js
+++ btjunkie.js
@@ -14,7 +14,7 @@ function parsebtjunkie(id,data){
    $(dataarray[index]).find('a').each(function(){
     title[index]=$(this).text();
    });
-   $(dataarray[index]).children('strong').each(function(){
+   $(dataarray[index]).children('b').each(function(){
     cat[index]=$(this).text();
     alert(cat[index]);
    });
