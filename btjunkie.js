699a5c70fa4eca7d5279da88fc27c134d141dcbd
immabear <crackopenabear@hotmail.com>
2010-10-12 13:27:27 -0700

    x

--- btjunkie.js
+++ btjunkie.js
@@ -11,17 +11,9 @@ function parsebtjunkie(id,data){
   if(index!==0&&index<((dataarray.length)-6)&&index<3){
    dataarray[index]='<p'+value;
    dataarray[index]=dataarray[index].replace(/p>(\s+)</g,'').replace(/\s+/g,' ').replace(/<\/strong>/,'<strong>|');
-   alert(dataarray[index]);
-   $(dataarray[index]).find(':nth-child(1)').each(function(){
-    alert('test');
-    title[index]=this.text();
-    loc[index]=$(this).attr('href');
-	alert(title[index]+'/n'+loc[index]);
-   });
    var temparray=$(dataarray[index]).text();
    title[index]=temparray.split('|')[0];
    loc[index]=dataarray[index].replace(/<a.*href=\"|\".*/g,'');
-      alert(loc[index]);
    temp=temparray.split('|')[1].split(' ');
    $.each(temp,function(i,val){
     switch(i){
@@ -84,54 +76,8 @@ function parsebtjunkie(id,data){
  $('#'+idobj.id+' div[id]').tooltip({effect: 'slide',offset: [27, 10],relative: 'true'});
  var dataarray=[], title=[], temp=[], seed=[], leech=[], cat=[], size=[], date=[], com=[], loc=[], tablerow, tablecd='';
 }
-function parsebtjunkie1(id,data){
- var title=[]; var seed=[]; var leech=[]; var cat=[]; var size=[]; var com=[];var loc=[];var tablerow; var tablecd='';
- $.each(data.query.results.item,function(index){
-  title[index]=(this).title.split("  [")[0];
-  loc[index]=(this).link.replace("http://btjunkie.org/","http://dl.btjunkie.org/")+"/download.torrent";
-  leech[index]=(this).title.split("  [")[1].split("/")[0];
-  seed[index]=(this).title.split("  [")[1].split("/")[1].split("]")[0];
-  cat[index]=(this).description.split(" ")[1];
-  size[index]=(this).description.split(" ")[3];
-  com[index]=
-   '<table style="margin:0;width:100%">'+
-    '<tr>'+
-	 '<td class="comoverlap">Seeds: '+seed[index]+' | Leechs: '+leech[index]+'</td>'+
-	'</tr>'+
-	'<tr>'+
-	 '<td class="comoverlap">Size: '+size[index]+'</td>'+
-	'</tr>'+
-	'<tr>'+
-	 '<td class="comoverlap">Category: '+cat[index]+'</td>'+
-	'</tr>'+
-   '</table>';
-  tablerow=
-   '<div id="result'+index+'" class="resultimgsm">'+
-   com[index]+
-   '</div>'+
-   '<div class="tooltipbig">'+
-    '<table>'+
-     '<tr>'+
-      '<td><b id="searchresult'+index+'">'+title[index]+'</b>'+
-	  '</td>'+
-	 '</tr>'+
-	 '<tr>'+
-	  '<td style="height:30px">'+
-	   '<button onclick="IMDb(\'searchresult'+index+'\')" class="buttonbig">IMDb</button>'+
-	   '<button onclick="downloadbtjunkie(\''+loc[index]+'\');" class="buttonbig">Info/Download</button>'+
-	  '</td>'+
-	 '</tr>'+
-	'</table>'+
-   '</div>';
-  tablecd=tablecd+tablerow; 
- });
- document.getElementById(id).innerHTML=tablecd;
- aligntd();
- $('#searchbox div[id]').tooltip({effect: 'slide',offset: [27, 10],relative: 'true'});
- var title=[]; var seed=[]; var leech=[]; var cat=[]; var size=[]; var com=[];var loc=[];var tablerow; var tablecd='';
-}
 function downloadbtjunkie(loc){
  minmaximize(0);
- document.getElementById('searchframe').innerHTML="<iframe src='"+loc+"' class='searchframe' frameborder='0'></frame>";
+ document.getElementById('searchframe').innerHTML="<iframe src='http://dl.btjunkie.org"+loc+"/download.torrent' class='searchframe' frameborder='0'></frame>";
  $("#searchframe").show(600);
 }
