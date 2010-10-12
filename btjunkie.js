be8aba56488d8de8010827947a2dae696a5df040
immabear <crackopenabear@hotmail.com>
2010-10-12 04:19:10 -0700

    v

--- btjunkie.js
+++ btjunkie.js
@@ -8,9 +8,12 @@ function parsebtjunkie(id,data){
  var dataarray=[], title=[], seed=[], leech=[], size=[], com=[], loc=[], tablerow, tablecd='', idobj=document.getElementById(id);
  dataarray=data.split("<p");
  $.each(dataarray,function(index, value){
-  if(index!==0&&index<10){
+  if(index!==0){
    dataarray[index]='<p'+value;
-   alert(dataarray[index]); 
+   $(dataarray[index]).find('p').each(function(){
+    title[index]=$(this).text();
+	alert(title[index]);
+   });
   }
  });
 }
