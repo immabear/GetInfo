21d86c9c9cf39fb21b6faf68cdc002238e12d22e
immabear <crackopenabear@hotmail.com>
2010-10-22 12:45:11 -0700

    x

--- tasks.js
+++ tasks.js
@@ -69,7 +69,7 @@ function minmaximize(x){
    $("#bodydiv").animate({'marginTop': '-30px', 'top': '100%', 'height': '30px'}, 600,
     function(){
      $("#minmax").text("Maximize");
-     $("#minmax").each(function(){(this).onclick=function(){minmaximize()}});
+     $("#minmax").onclick=function(){minmaximize()};
 	}
    );
    break;
@@ -78,7 +78,7 @@ function minmaximize(x){
    $("#bodydiv").animate({'marginTop': '-300px', 'top': '50%', 'height': '600px'}, 600,
     function(){
      $("#minmax").text("Minimize");
-     $("#minmax").each(function(){(this).onclick=function(){minmaximize(0)}});
+     $("#minmax").onclick=function(){minmaximize(0)};
 	}
    );
  }
