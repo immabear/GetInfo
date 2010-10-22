8654684e53cf9d46ddb852e13e2dad6ec1a58e7e
immabear <crackopenabear@hotmail.com>
2010-10-21 23:53:28 -0700

    x

--- nyaatorrents.js
+++ nyaatorrents.js
@@ -65,7 +65,7 @@ function parsenyaatorrents(id,data){
 	  '<tr>'+
 	   '<td style="height:30px">'+
 	    '<button onclick="IMDb(\'searchresult'+index+'\')" class="buttonbig">IMDb</button>'+
-	    '<button onclick="downloadbtjunkie(\''+loc[index]+'\');" class="buttonbig">Info/Download</button>'+
+	    '<button onclick="downloadnyaatorrents(\''+loc[index]+'\');" class="buttonbig">Info/Download</button>'+
 	   '</td>'+
 	  '</tr>'+
 	 '</table>'+
@@ -79,79 +79,8 @@ function parsenyaatorrents(id,data){
  $('#'+idobj.id+' div[id]').tooltip({effect: 'slide',offset: [27, 10],relative: 'true'});
  var dataarray=[], title=[], seed=[], leech=[], cat=[], size=[], com=[], loc=[], dls=[], tablerow, tablecd='';
 }
-
-function parsebtjunkie00(id,data){
- var dataarray=[], title=[], temp=[], seed=[], leech=[], cat=[], size=[], date=[], com=[], loc=[], tablerow, tablecd='', idobj=document.getElementById(id);
- dataarray=data.split("<p");
- $.each(dataarray,function(index, value){
-  if(index!==0&&index<((dataarray.length)-6)){
-   dataarray[index]='<p'+value;
-   dataarray[index]=dataarray[index].replace(/p>(\s+)</g,'').replace(/\s+/g,' ').replace(/<\/strong>/,'<strong>|||');
-   var temparray=$(dataarray[index]).text();
-   title[index]=temparray.split('|||')[0];
-   loc[index]=dataarray[index].replace(/<a.*href=\"|\".*/g,'');
-   temp=temparray.split('|||')[1].split(' ');
-   $.each(temp,function(i,val){
-    switch(i){
-	 case 0:
-	  cat[index]=val;
-	  break;
-     case 1:
-	  size[index]=val;
-	  break;
-     case 2:
-	  date[index]=val;
-	  break;
-     case 3:
-	  if(val&&val!==' ')seed[index]=val;
-	  else seed[index]='X';
-	  leech[index]='X';
-	  break;
-     case 4:
-	  if(val&&val!==' ')leech[index]=val;
-	  else leech[index]='X';
-	  break;
-	}
-   });
-   com[index]=
-   '<table>'+
-    '<tr>'+
-     '<td class="comoverlap">Seeds: '+seed[index]+' | Leechs: '+leech[index]+'</td>'+
-    '</tr>'+
-    '<tr>'+
-     '<td class="comoverlap">Size: '+size[index]+'</td>'+
-    '</tr>'+
-    '<tr>'+
-     '<td class="comoverlap">Category: '+cat[index]+'</td>'+
-    '</tr>'+
-    '<tr>'+
-     '<td class="comoverlap">Date: '+date[index]+'</td>'+
-    '</tr>'+
-   '</table>';
-   tablerow=
-    '<div id="result'+index+'" class="resultimgsm">'+
-     com[index]+
-    '</div>'+
-    '<div class="tooltipbig">'+
-     '<table>'+
-      '<tr>'+
-       '<td><b id="searchresult'+index+'">'+title[index]+'</b>'+ 
-       '</td>'+
-	  '</tr>'+
-	  '<tr>'+
-	   '<td style="height:30px">'+
-	    '<button onclick="IMDb(\'searchresult'+index+'\')" class="buttonbig">IMDb</button>'+
-	    '<button onclick="downloadbtjunkie(\''+loc[index]+'\');" class="buttonbig">Info/Download</button>'+
-	   '</td>'+
-	  '</tr>'+
-	 '</table>'+
-    '</div>';
-   tablecd=tablecd+tablerow;
-  }
- });
- idobj.innerHTML=tablecd;
- aligntd();
- $('#'+idobj.id+' div[id]').last().css('margin-bottom', '10px');
- $('#'+idobj.id+' div[id]').tooltip({effect: 'slide',offset: [27, 10],relative: 'true'});
- var dataarray=[], title=[], temp=[], seed=[], leech=[], cat=[], size=[], date=[], com=[], loc=[], tablerow, tablecd='';
+function downloadnyaatorrents(loc){
+ minmaximize(0);
+ document.getElementById('searchframe').innerHTML="<iframe src='"+loc+"' class='searchframe' frameborder='0'></frame>";
+ $("#searchframe").show(600);
 }
